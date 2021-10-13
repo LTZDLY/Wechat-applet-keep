af698f62aa50fcb90161f49aaddfbaae6ea7dbc3
unknown <unknown@example.com>
2021-10-13 23:28:26 +0800

    添加标题栏，添加几个页面，粗略绘制页面

---
+++ tabs/index/index.js
@@ -0,0 +1,15 @@
+Page({
+  onShow(){
+    if (typeof this.getTabBar === 'function' &&
+    this.getTabBar()) {
+      this.getTabBar().setData({
+        selected: 0
+      })
+    }
+  },
+  bindViewTap() {
+    wx.navigateTo({
+      url: '../../pages/search/search'
+    })
+  }
+})
